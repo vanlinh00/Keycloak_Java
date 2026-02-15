e06aa815b03a826a15277f17607064c8f6e50ed7
vanlinh00 <vanlinh20192019@gmail.com>
2026-02-16 01:09:04 +0700

    first commit

---
+++ src/main/java/org/example/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package org.example.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from your new Web Project!";
+    }
+}
